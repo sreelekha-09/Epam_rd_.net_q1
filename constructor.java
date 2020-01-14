3e0276e4d8368e5bc5d9a5d6751e981321d67c95
sreelekha-09 <59833367+sreelekha-09@users.noreply.github.com>
2020-01-14 08:24:04 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -4,11 +4,12 @@ public class constructor {
 	int a;
 	int b;
 	String c;
+	double d;
  constructor()
  {
 	 System.out.println("constructor called");
  }
- constructor(int a ,int b)
+public constructor(int a ,int b)
  {
 	 this("name");
 	 this.a=a;
@@ -18,10 +19,28 @@ public class constructor {
  {
 	 this.c=c;
  }
+protected constructor(double d)
+ {
+	 this.d=d;
+ }
  public static void main(String gARgs[])
  {
 	 constructor c=new constructor();
 	 constructor c1=new constructor(23,67);
 			 
  }
+}
+public class cons extends constructor {
+	public static void main(String Args[])
+	{
+		constructor obj=new constructor(89.2);
+		
+	}
+}
+
+public class cons_public {
+public static void main(String Args[])
+{
+	constructor o=new constructor(4,6);
+}
 }
